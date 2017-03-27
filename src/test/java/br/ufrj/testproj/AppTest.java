e362601ec1af43c69d151217ca060868e6744d9f
Juan Augusto <juan.augusto@ufrj.br>
2017-03-26 23:19:35 -0300

    First commit for Projeto de Teste de Software - MAB640

---
+++ src/test/java/br/ufrj/testproj/AppTest.java
@@ -0,0 +1,41 @@
+package br.ufrj.testproj;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+	
+	public void testCompareMsgs(){
+		String x = "a";
+		String y = "a";
+		
+		assertEquals(x, y);
+	}
+}
